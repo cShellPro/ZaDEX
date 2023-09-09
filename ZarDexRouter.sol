9730497e937c96be9c769cbb63d32ed0124fd9f6
cShellPro <79190115+cShellPro@users.noreply.github.com>
2023-09-09 16:26:03 +0200

    Update ZarDexRouter.sol

--- ZarDexRouter.sol
+++ ZarDexRouter.sol
@@ -3,11 +3,11 @@ pragma solidity =0.6.6;
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
 
-import './interfaces/IUniswapV2Router02.sol';
-import './libraries/UniswapV2Library.sol';
-import './libraries/SafeMath.sol';
-import './interfaces/IERC20.sol';
-import './interfaces/IWETH.sol';
+import './IUniswapV2Router02.sol';
+import './UniswapV2Library.sol';
+import './SafeMath.sol';
+import './IERC20.sol';
+import './IWETH.sol';
 
 contract ZarDexRouter is IUniswapV2Router02 {
     using SafeMath for uint;
